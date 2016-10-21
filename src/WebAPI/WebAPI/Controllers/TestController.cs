ece3b77e8bf126f37e46c6863cbc98cdc2ccd2d4
jamisliao <jamisliao@gmail.com>
2016-10-21 11:47:55 +0800

    新增多工加密

--- src/WebAPI/WebAPI/Controllers/TestController.cs
+++ src/WebAPI/WebAPI/Controllers/TestController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
 using WebAPI.Models;
 
@@ -28,6 +32,50 @@ namespace WebAPI.Controllers
                 IsSuccess = true
             };
 
+            Delay();
+            return result;
+        }
+
+        private void Delay()
+        {
+            long i = 0;
+            Task t = new Task(() =>
+            {
+                Parallel.For(1, 10000, _ =>
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        EncryptAES(Guid.NewGuid().ToString("N"));
+                    }
+                });
+
+            });
+
+            t.Start();
+
+
+            Task.WaitAll(t);
+        }
+
+        private string EncryptAES(string text)
+        {
+            string result = "";
+            System.Security.Cryptography.AesCryptoServiceProvider provider = new System.Security.Cryptography.AesCryptoServiceProvider();
+            provider.IV = Convert.FromBase64String("xJt2LaCGtAQB3+3edKlbaw==");
+            provider.Key = Convert.FromBase64String("pJ7JyvLQDPTTIQ4D9x9bZhxrxZY7faGB4Q8PvgmwRrI=");
+            provider.Mode = System.Security.Cryptography.CipherMode.CBC;
+            provider.Padding = System.Security.Cryptography.PaddingMode.PKCS7;
+            byte[] dataByteArray = Encoding.Unicode.GetBytes(text);
+            // encryption
+            using (System.Security.Cryptography.ICryptoTransform encrypt = provider.CreateEncryptor())
+            {
+                byte[] dest = encrypt.TransformFinalBlock(dataByteArray, 0, dataByteArray.Length);
+
+                // Convert byte array to Base64 strings
+                result = Convert.ToBase64String(dest);
+            }
+
+            Debug.WriteLine(result);
             return result;
         }
     }
